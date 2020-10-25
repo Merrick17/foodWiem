92a43f6254a7b20fb5799845e1899ad45861109d
=Safwen <safweb.benfredj@gmail.com>
2020-10-25 14:41:26 +0100

    convert to class

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { ToastProvider, useToasts } from 'react-toast-notifications'
 import './App.css'
 import Map from './components/Map'
-function App() {
-  return (
-    <ToastProvider placement="top-center">
-      <Fragment>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Map} />
-          </Switch>
-        </Router>
-      </Fragment>
-    </ToastProvider>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <ToastProvider placement="top-center">
+        <Fragment>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Map} />
+            </Switch>
+          </Router>
+        </Fragment>
+      </ToastProvider>
+    )
+  }
 }
 
 export default App
